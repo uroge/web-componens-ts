e8be38a06142be4b0aac0a90cb7c770c99e6cbac
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-02-24 17:44:10 +0100

    Restructure css and setup basic css variables

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-import './style.css';
+import './custom-select/custom-select';
+
+function switchTheme() {
+  const body = document.body;
+  body.classList.toggle('light-theme');
+}
+
+const themeSwitcher = document.getElementById('theme-switch');
+themeSwitcher?.addEventListener('click', switchTheme);
